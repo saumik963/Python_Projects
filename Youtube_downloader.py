361a1abde71609791d4c21f2583a0b282d905ca6
Saumik Das <119744035+saumik963@users.noreply.github.com>
2024-02-05 19:30:54 +0600

    Update Youtube_downloader.py v1.1

--- Youtube_downloader.py
+++ Youtube_downloader.py
@@ -1,7 +1,9 @@
 import tkinter
 import customtkinter as ct
 from pytube import YouTube
-import os
+import os,webbrowser
+import ctypes
+
 
 
 # function area
@@ -14,10 +16,10 @@ def startDownload():
     try:
         ytlink = link.get()
         ytobj = YouTube(ytlink,on_progress_callback=on_progress)
+        path=path_loc.get()
 
         if res_op.get()=='mp3':
-            video = ytobj.streams.filter(only_audio=True).first()
-            filename = ytobj.title + '.mp3'
+            video = ytobj.streams.get_audio_only()
 
             filesize_mb = video.filesize / (1024 * 1024)
 
@@ -26,7 +28,6 @@ def startDownload():
             file_size.configure(text=f'Size : {filesize_mb:.2f} MB' ,text_color='green')
 
 
-            video.download(os.getcwd(),filename=filename)
             
         else:
             video = ytobj.streams.filter(res = res_op.get(), file_extension = "mp4").first()
@@ -35,7 +36,7 @@ def startDownload():
             title.configure(text=ytobj.title,text_color='green')
             file_size.configure(text=f'Size : {filesize_mb:.2f} MB' ,text_color='green')
 
-            video.download(os.getcwd())
+        video.download(path)
 
 
 
@@ -62,11 +63,17 @@ ct.set_appearance_mode("System")
 ct.set_default_color_theme("blue")
 
 app= ct.CTk()
-app.geometry("400x250")
+app.geometry("400x290")
 app.title("YT Downloader")
 
+# taskbar logo
+myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
-title= ct.CTkLabel(app,text="Insert YouTube Video Link")
+app.iconbitmap(r'C:\Users\Saumik\Desktop\Python\Python practices\YTD\youtube-logo.ico')
+
+
+title= ct.CTkLabel(app,text="Insert YouTube Video Link",font=("Protest Strike",14))
 title.pack(padx=10,pady=10)
 
 url_var=tkinter.StringVar()
@@ -95,11 +102,26 @@ download= ct.CTkButton(app,text="Download" , command=startDownload)
 download.place(x=130,y=170)
 
 finishLabel= ct.CTkLabel(app,text="")
-finishLabel.place(x=140,y=210)
+finishLabel.place(x=140,y=200)
 
 file_size= ct.CTkLabel(app,text="")
 file_size.place(x=310,y=95)
 
+# path choose
+
+pathLabel= ct.CTkLabel(app,text="Download to ")
+pathLabel.place(x=25,y=232)
+
+path_address=tkinter.StringVar(value=os.getcwd())
+path_loc=ct.CTkEntry(app,width=280,height=30,textvariable=path_address)
+path_loc.place(x=100,y=230)
+
+
+# About
+
+about= ct.CTkLabel(app,text="Created By SDA",text_color='green',cursor='hand2',font=('Orbitron',11,'bold'))
+about.place(x=290,y=262)
+about.bind('<Button-1>',lambda x:webbrowser.open_new("https://saumik963.pythonanywhere.com/"))
 
 app.resizable(False,False)
 app.mainloop()
